fbcb72229360e7e2a2e9171c3ee393f45ce34483
nathanbuyr <G00421082@atu.com>
2024-12-26 03:49:18 +0000

    installed nodemon, created index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser');
+var express = require('express')
+var mongoDao = require('./mongoDao')
+var app = express()
+app.set('view engine', 'ejs') //setting render engine
+app.use(bodyParser.urlencoded({extended: false}))
+const { check, validationResult } = require('express-validator');
+
+app.listen(3004, () => {
+    console.log("Port is listening on 3004")
+})
