27b11faaf87003305d7fa5751779aa0f9681300a
nathanbuyr <G00421082@atu.com>
2024-12-26 04:07:48 +0000

    Hyperlinks for students,grades and lecturers created, all redirect to respective pages

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ app.listen(3004, () => {
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/students", (req, res) => {
+    res.send("<h1>Students Page</h1>");
+});
+
+app.get("/grades", (req, res) => {
+    res.send("<h1>Grades Page</h1>");
+});
+
+app.get("/lecturers", (req, res) => {
+    res.send("<h1>Lecturers Page</h1>");
+});
